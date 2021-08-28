132c04ac98c919247a8649964e52dec9fda5239a
4jonathanfoster <82682840+4jonathanfoster@users.noreply.github.com>
2021-08-27 21:37:49 -0500

    mutations

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import {ADD_USER} from '../utils/mutations'
 
 const SignupForm = () => {
   // set initial form state
@@ -11,6 +13,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,16 +31,27 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      // const response = await createUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
-      const { token, user } = await response.json();
-      console.log(user);
+      // const { token, user } = await response.json();
+      // console.log(user);
+      // Auth.login(token);
+      const mutationResponse = await addUser({
+        variables: {
+          email: userFormData.email,
+          password: userFormData.password,
+          username: userFormData.username,
+
+        },
+      });
+      const token = mutationResponse.data.addUser.token;
       Auth.login(token);
     } catch (err) {
+      console.log('ERRRRR', err)
       console.error(err);
       setShowAlert(true);
     }
---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,72 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: String
+  }
+
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    category: Category
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
+
+  type Book {
+      authors: [String]
+     description: String
+     bookid: String
+     image: String 
+     link: String
+     title: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID
+
+  }
+
+  type Query {
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+  }
+
+  type Mutation {
+    addUser(
+      username: String!
+      email: String!
+      password: String!
+    ): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(
+      firstName: String
+      lastName: String
+      email: String
+      password: String
+    ): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
